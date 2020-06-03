758596a0b5f3ad97d255b376869a23be2f49d1aa
Sophie Carlsson <44728013+itssophiecarlsson@users.noreply.github.com>
2020-06-03 21:53:42 +0200

    Inte klar, men nästan ;)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-<script>
-</script>
+//Newsletter Sign-Up Forum Code, unsure if it works.
+
+var modal = document.getElementById('form');
+{
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
